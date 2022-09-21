61f1680bdb826a231ebdadd761c5c74bd2f290a7
Abhi Yerra <abhi@berkeley.edu>
2022-09-20 17:30:50 -0700

    Added ssm

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-provider "aws" {
-  alias   = "ssm"
-  profile = "<profile>"
-  region  = "us-east-1"
-}
-
 data "aws_ssm_parameter" "secret" {
-  provider        = aws.ssm
   name            = var.secret
   with_decryption = true
 }
